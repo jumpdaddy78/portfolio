352fdd1c9de2541a9bf38e2ef5b1b2e161c70024
marc Springer <hackcheck78@gmail.com>
2023-10-26 18:02:33 +0200

    C:/Program Files/Git/contact route added, header and footer as template

--- main.py
+++ main.py
@@ -10,11 +10,15 @@ name = "Marc Springer"
 
 skills = ["AWS", "Git", "Bash"]
 
+contact = "marc.springer@docc.techstarter.de"
 
 
 @app.get("/", response_class=HTMLResponse)
 async def get_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "name" : name, "skills": skills})
 
+@app.get("/contact", response_class=HTMLResponse)
+async def get_contact(request: Request):
+    return templates.TemplateResponse("contact.html", {"request": request,})
 
 
